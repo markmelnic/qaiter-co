0023646739268c6d8b115b40bf7fe6064702698c
markmelnic <markhappy200@gmail.com>
2021-09-13 21:41:41 +0300

    Minor improvements

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,8 @@
-try:
-    import envars
-except ImportError:
-    pass
-
-import os
+from dotenv import load_dotenv
+load_dotenv()
 
 from app import app
 
 if __name__ == "__main__":
-    import gunicorn, psycopg2, pypugjs, pretty_errors, png
+    import gunicorn, psycopg2, pypugjs, pretty_errors, dotenv
     app.run(debug=True)
